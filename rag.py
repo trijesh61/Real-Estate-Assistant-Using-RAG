118230f95b597c0d7ec9a66d8105d077ca078a32
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2026-05-24 15:31:05 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,8 +1,40 @@
+from dotenv import load_dotenv
+from pathlib import Path
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings  
+
+load_dotenv()
+
+#constants
+chunk_size=1000
+
 
 def process_urls(urls):
-    pass
+    # Document Loading From Urls
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Referer": "https://www.google.com/",
+    }
+
+    loader=UnstructuredURLLoader(urls=urls,headers=headers)
+    data=loader.load()
+    
+    # Document Splitting
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n","\n","."," "],chunk_size=200)
+    docs = text_splitter.split_documents(data)
+
+
 
 if __name__=="__main__":
-    urls=[]
+    urls = [
+        "https://www.cnbc.com/2024/12/21/how-the-federal-reserves-rate-policy-affects-mortgages.html",
+        "https://www.cnbc.com/2024/12/20/why-mortgage-rates-jumped-despite-fed-interest-rate-cut.html"
+    ]
     
     process_urls(urls)
