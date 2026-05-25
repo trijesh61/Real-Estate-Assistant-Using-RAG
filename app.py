427ca89df7d7b5f4896d426f927feb32ae243df1
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2026-05-25 22:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ url3=st.sidebar.text_input("URL 3")
 placeholder=st.empty()
 process_url_button=st.sidebar.button("Process URLs")
 if process_url_button:
-    urls = [url for url in (url1, url2, url3) if url is not None]
+    urls = [url for url in (url1, url2, url3) if url!=""]
     if len(urls) == 0:
         placeholder.text("You must provide at least one valid url")
     else:
         for status in process_urls(urls):
             placeholder.text(status)
+
+query=st.text_input("Question")
+if query:
+    answer,source=generate_answer(query=query)
+    print(answer)
